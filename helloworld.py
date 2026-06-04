3a36044b0fdec60640993c59cb08604463aa3aae
EastonWilliam <mainline.work536@gmail.com>
2026-06-04 08:25:23 -0400

    Add Encode

--- helloworld.py
+++ helloworld.py
@@ -22,3 +22,5 @@ print(type((9.8, 3.14, 2.7)))    # Tuple
 print(type(3 == 3))              # Bool
 print(type(3 >= 3))              # Bool
 print(type(3 <= 2))              # Bool
+
+//EncodingWarning
